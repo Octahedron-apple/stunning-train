bc4f0043d485d6f86ef879451caba61d8caf2072
Octahedron-apple <bhavya1singh1@gmail.com>
2026-05-19 14:48:35 +0530

    re did reader

--- src/stunning_train/core.py
+++ src/stunning_train/core.py
@@ -1,2 +1,21 @@
 import curses
 import time
+import io
+class reader:
+    def file_stream_gen(self,obj):
+        for line in obj:
+            for word in line.split():
+                yield word
+    def __init__(self, stream, wpm=300, chunk_size= 5):
+        self.wpm= wpm
+        self.chunk_size = chunk_size
+        if isinstance(stream, str):
+            self.stream=iter(stream.split())
+        elif isinstance(stream, io.IOBase):
+            self.stream = self.file_stream_gen(stream)
+        else:
+            raise TypeError("Stream must be a string or a file-like object")
+        self.history = []
+        self.lookahead = []
+        self.current = None
+        
